60ee64c5949641f8df5253478d455b413777b485
aliakseiTraihel <94895287+aliakseiTraihel@users.noreply.github.com>
2023-12-25 20:25:39 +0100

    fetch: registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useRef, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    accept: false
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const handleValidation = () => {
+    let valid = true;
+    const refs: React.RefObject<HTMLInputElement>[] = [];
+
+    if (!formData.name || (formData.name && formData.name.length < 6)) {
+      setErrors((errors) => ({...errors, name: 'Min name length 6 charachters'}))
+      valid = false;
+      refs.push(nameRef);
+    } else {
+      setErrors((errors) => ({...errors, name: ''}))
+    }
+
+    if (!formData.email || (formData.email && !formData.email.includes('@'))) {
+      setErrors((errors) => ({...errors, email: 'Email is not valid'}))
+      valid = false;
+      refs.push(emailRef);
+    } else {
+      setErrors((errors) => ({...errors, email: ''}))
+    }
+
+    if (!formData.password || (formData.password && formData.password.length < 6)) {
+      setErrors((errors) => ({...errors, password: 'Min password length 6 charachters'}))
+      valid = false;
+      refs.push(passwordRef);
+    } else {
+      setErrors((errors) => ({...errors, password: ''}))
+    }
+
+    if(refs[0] && refs[0].current) {
+      refs[0].current.focus()
+    }
+
+    return valid;
+  }
+
+  const handleForm = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (handleValidation()) {
+      clearForm()
+      alert("Success")
+    }
+  }
+
+  const clearForm = () => {
+    setFormData({
+      name: '',
+      email: '',
+      password: '',
+      accept: false
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <form noValidate className="form" onSubmit={handleForm}>
+        <div>
+          <label htmlFor="name">
+            Name:&nbsp;
+            <input onChange={(e) => setFormData({...formData, name: e.target.value})} required
+            ref={nameRef}
+            name="name"
+            value={formData.name}
+            placeholder="name" />
+            {errors.name && <p style={{ color: 'red' }}>{errors.name}</p>}
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email:&nbsp;
+            <input onChange={(e) => setFormData({...formData, email: e.target.value})} required
+            ref={emailRef}
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="example@mail.com" />
+            {errors.email && <p style={{ color: 'red' }}>{errors.email}</p>}
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">
+            Password:&nbsp;
+          <input onChange={(e) => setFormData({...formData, password: e.target.value})} required
+            ref={passwordRef}
+            type="password"
+            name="password"
+            value={formData.password}
+            placeholder="password" />
+           {errors.password && <p style={{ color: 'red' }}>{errors.password}</p>}
+          </label>
+        </div>
+        <div>
+          <label htmlFor="accept">
+            Accept Terms:&nbsp;
+          <input onChange={(e) => setFormData({...formData, accept: e.target.checked})} required
+            type="checkbox"
+            checked={formData.accept}
+            name="accept"
+            id="accept"/>
+          </label>
+        </div>
+        <div>
+          <button disabled={!formData.accept} type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
