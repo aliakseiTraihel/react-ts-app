81d8abf30d82837c7c1a789bc1eb307a337ac002
aliakseiTraihel <94895287+aliakseiTraihel@users.noreply.github.com>
2024-02-09 13:33:25 +0100

    fetch: updates

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -1,14 +1,25 @@
 import './Task.css'
+import {useState} from "react";
 
 interface Task {
   text: string,
-  active: false
+  active: boolean
 }
 
-function Task({task, callback, selected}: {task: Task, callback: (task: Task)=> void, selected: boolean}) {
+function Task({task, callback}: {task: Task, callback: (task: Task) => void}) {
+
+  const [selected, setSelected] = useState(false)
+
+  const handleOnClick = () => {
+    setSelected(!selected);
+    (function () {
+      callback(task)
+    })()
+  }
+
   return(
     <div className={selected ? 'selected ' : '' + task.active ? 'active' : ''}>
-      <h3 onClick={() => callback(task)}>{task.text}</h3>
+      <h3 onClick={handleOnClick}>{task.text}</h3>
     </div>
   );
 }
--- src/components/tasks/Tasks.tsx
+++ src/components/tasks/Tasks.tsx
@@ -3,38 +3,55 @@ import Task from "./Task.tsx";
 import {useCallback, useEffect, useState} from 'react';
 
 const init: Task[] = [
-  {text: 'first', active: false}
+  {text: 'first', active: true},
+  {text: 'second', active: false}
 ]
 
 function Tasks(){
   
-  const [ active, setActive ] = useState<Task[]>([]);
-  const [ done, setDone ] = useState<Task[]>([]);
+  const [ todos, setTodos ] = useState<Task[]>([]);
   const [ selected, setSelected] = useState<Task>();
   
   const handleCallback = useCallback((task: Task) => {
-    console.log(task)
     setSelected(task);
   }, []);
   
-  const handleClick = (e: KeyboardEvent) => {
-    console.log(e);
-    console.log(selected);
+  const handlePress = (e: KeyboardEvent) => {
+    if (!selected) {
+      return;
+    }
+
+    switch (e.code) {
+      case "ArrowRight":
+        if (selected.active) {
+          setTodos([...todos, {...selected, active: false}]);
+        }
+        break;
+      case "ArrowLeft":
+        if (!selected.active) {
+          setTodos([...todos, {...selected, active: true}]);
+        }
+        break;
+      default:
+        break;
+    }
   }
   
   useEffect(() => {
-    document.addEventListener("keydown", handleClick);
-    setActive(init)
+    document.addEventListener("keydown", handlePress);
     return () => {
-      document.removeEventListener("keydown", handleClick);
+      document.removeEventListener("keydown", handlePress);
     }
+  }, [selected])
+
+  useEffect(() => {
+    setTodos(init)
   }, [])
 
   return (
-    <div>
-      <h1>Test</h1>
-      <div>{active.map((task, index) => <Task key={index} task={task} callback={handleCallback} selected={false}/>)}</div>
-      <div>{done.map((task, index) => <Task key={index} task={task} callback={handleCallback} selected={false}/>)}</div>
+    <div className="task-container">
+      <div className="active">{todos.filter(task => task.active).map((task, index) => <Task key={index} task={task} callback={handleCallback}/>)}</div>
+      <div className="done">{todos.filter(task => !task.active).map((task, index) => <Task key={index} task={task} callback={handleCallback}/>)}</div>
     </div>
   );
 }
